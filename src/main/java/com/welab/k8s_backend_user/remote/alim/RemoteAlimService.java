dd7f75820652da16cbae856007d79bef6f4b35a9
Hwanjin-Choi <68840464+Hwanjin-Choi@users.noreply.github.com>
2025-06-11 16:19:25 +0900

    Update RemoteAlimService.java

--- src/main/java/com/welab/k8s_backend_user/remote/alim/RemoteAlimService.java
+++ src/main/java/com/welab/k8s_backend_user/remote/alim/RemoteAlimService.java
@@ -5,7 +5,6 @@ import lombok.NoArgsConstructor;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@NoArgsConstructor
 @FeignClient(name = "RemoteAlimService", url = "http://k8s-backend-alim-service:8080", path = "/backend/alim/v1")
 public interface RemoteAlimService {
     @GetMapping(value = "/hello")
